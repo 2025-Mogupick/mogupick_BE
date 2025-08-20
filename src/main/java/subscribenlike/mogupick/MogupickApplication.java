2f6391c97fb37be78c97bb983f98552ab5fe329e
0tae <choiyt3465@naver.com>
2025-08-20 09:51:09 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/subscribenlike/mogupick/MogupickApplication.java
@@ -0,0 +1,13 @@
+package subscribenlike.mogupick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogupickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MogupickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/subscribenlike/mogupick/MogupickApplicationTests.java
@@ -0,0 +1,13 @@
+package subscribenlike.mogupick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogupickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
