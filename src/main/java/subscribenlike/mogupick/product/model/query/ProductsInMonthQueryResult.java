497463886f856a9bcf529e814137e7fdf70da50b
0tae <choiyt3465@naver.com>
2025-08-29 13:16:05 +0900

    feat : 월별 상품 데이터베이스 쿼리 조회 dto 구현

---
+++ src/main/java/subscribenlike/mogupick/product/model/query/ProductsInMonthQueryResult.java
@@ -0,0 +1,20 @@
+package subscribenlike.mogupick.product.model.query;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ProductsInMonthQueryResult {
+    private Long productId;
+    private String productImageUrl;
+    private String productName;
+    private Integer productPrice;
+    private LocalDateTime createdAt;
+    private Long brandId;
+    private String brandName;
+    private Double rating;
+    private Long reviewCount;
+}
