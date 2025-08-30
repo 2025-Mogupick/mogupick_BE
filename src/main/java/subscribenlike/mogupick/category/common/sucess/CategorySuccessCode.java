11989a653519ecc0e8b42506aff8c1881641abfd
0tae <choiyt3465@naver.com>
2025-08-30 19:07:45 +0900

    fix : 의도한 응답을 반환하도록 코드 수정

--- src/main/java/subscribenlike/mogupick/category/common/sucess/CategorySuccessCode.java
+++ src/main/java/subscribenlike/mogupick/category/common/sucess/CategorySuccessCode.java
@@ -10,9 +10,7 @@ import subscribenlike.mogupick.common.success.SuccessCode;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum CategorySuccessCode implements SuccessCode {
     ROOT_CATEGORIES_FETCHED(HttpStatus.OK, "루트 카테고리 리스트 조회에 성공하였습니다."),
-    CATEGORY_OPTIONS_AND_FILTERS_FETCHED(HttpStatus.OK, "카테고리 옵션 및 필터 조회에 성공하였습니다.")
-
-    ;
+    CATEGORY_OPTIONS_AND_FILTERS_FETCHED(HttpStatus.OK, "카테고리 옵션 및 필터 조회에 성공하였습니다.");
 
     private final HttpStatus status;
     private final String message;
