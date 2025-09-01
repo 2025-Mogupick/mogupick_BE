70d1f2238590c89b76033d8e3a38099e7fa5dbbc
0tae <choiyt3465@naver.com>
2025-09-01 16:58:26 +0900

    feat : 내 또래 베스트 리뷰 조회 쿼리 작성

--- src/main/java/subscribenlike/mogupick/product/repository/ProductRepository.java
+++ src/main/java/subscribenlike/mogupick/product/repository/ProductRepository.java
@@ -2,9 +2,12 @@ package subscribenlike.mogupick.product.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import subscribenlike.mogupick.product.common.ProductErrorCode;
 import subscribenlike.mogupick.product.common.ProductException;
 import subscribenlike.mogupick.product.domain.Product;
+import subscribenlike.mogupick.product.model.query.FetchPeerBestReviewsQueryResult;
 
 import java.util.List;
 import java.util.Map;
@@ -24,4 +27,44 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Product
 
     List<Product> findByNameContainingIgnoreCase(String keyword);
 
+    @Query(value = """
+        SELECT
+            p.id AS productId,
+            b.name AS brandName,
+            p.name AS productName,
+            p.price AS price,
+            YEAR(m.birth_date) AS birth_year,
+            m.profile_image AS profileImage,
+            m.name AS memberName,
+            r.review_image AS reviewImage,
+            r.created_at AS createdAt,
+            COALESCE(review_likes.review_like, 0) AS likeCount,
+            COALESCE(review_counts.review_count, 0) AS reviewCount
+        FROM review r
+        JOIN member m ON r.member_id = m.id
+        JOIN product p ON r.product_id = p.id
+        JOIN brand b ON b.id = p.brand_id
+        LEFT JOIN (
+            SELECT
+                review_likes.review_id,
+                COUNT(*) AS review_like
+            FROM review_likes
+            GROUP BY review_id
+        ) AS review_likes ON r.id = review_likes.review_id
+        LEFT JOIN (
+            SELECT
+                review.product_id,
+                COUNT(*) AS review_count
+            FROM review
+            GROUP BY product_id
+        ) AS review_counts ON p.id = review_counts.product_id
+        WHERE YEAR(m.birth_date) BETWEEN :fromYear AND :toYear
+        ORDER BY likeCount DESC
+        LIMIT :limit
+    """, nativeQuery = true)
+    List<FetchPeerBestReviewsQueryResult> fetchPeerBestReviewNative(
+            @Param("fromYear") int fromYear,
+            @Param("toYear") int toYear,
+            @Param("limit") int limit
+    );
 }
