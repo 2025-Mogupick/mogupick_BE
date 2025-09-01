bd2ac512ecfcb41acfe6dd09bfb9da2f0c300e07
SeongJun98 <seongjunin98@gmail.com>
2025-09-01 10:59:03 +0900

    fix: 로그인 성공 시 DB에 Refresh Token 저장

--- src/main/java/subscribenlike/mogupick/global/oauth/OAuth2LoginSuccessHandler.java
+++ src/main/java/subscribenlike/mogupick/global/oauth/OAuth2LoginSuccessHandler.java
@@ -5,11 +5,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken; // ✅ import 추가
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.util.UriComponentsBuilder;
 import subscribenlike.mogupick.global.jwt.JwtProvider;
 import subscribenlike.mogupick.global.jwt.TokenInfo;
+import subscribenlike.mogupick.member.domain.Member;
+import subscribenlike.mogupick.member.repository.MemberRepository;
 
 import java.io.IOException;
 
@@ -18,10 +23,22 @@ import java.io.IOException;
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtProvider jwtProvider;
+    private final MemberRepository memberRepository;
 
     @Override
+    @Transactional
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         TokenInfo tokenInfo = jwtProvider.generateToken(authentication);
+
+        String registrationId = ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId();
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, oAuth2User.getAttributes());
+        String email = attributes.getEmail();
+
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        member.updateRefreshToken(tokenInfo.getRefreshToken());
 
         String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/oauth-redirect")
                 .queryParam("accessToken", tokenInfo.getAccessToken())
