86e8ba813275af7002bd9741a55d00a3d6acd61e
0tae <choiyt3465@naver.com>
2025-09-03 21:06:43 +0900

    feat/62: Implement member recently viewed products API with MemberProductViewCount

--- src/main/java/subscribenlike/mogupick/product/repository/MemberProductViewCountRepository.java
+++ src/main/java/subscribenlike/mogupick/product/repository/MemberProductViewCountRepository.java
@@ -1,17 +1,13 @@
 package subscribenlike.mogupick.product.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import subscribenlike.mogupick.product.common.ProductErrorCode;
-import subscribenlike.mogupick.product.common.ProductException;
 import subscribenlike.mogupick.product.domain.MemberProductViewCount;
-import subscribenlike.mogupick.product.domain.Product;
-import subscribenlike.mogupick.product.model.query.FetchPeerBestReviewsQueryResult;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 public interface MemberProductViewCountRepository extends JpaRepository<MemberProductViewCount, Long>, MemberProductViewCountQuerydslRepository {
 
+    Optional<MemberProductViewCount> findByMemberIdAndProductId(Long memberId, Long productId);
+
+    boolean existsByMemberIdAndProductId(Long memberId, Long productId);
 }
