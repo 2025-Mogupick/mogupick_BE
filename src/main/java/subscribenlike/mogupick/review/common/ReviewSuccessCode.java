67ead9faac650d1147e27e106458425e23f00560
0tae <choiyt3465@naver.com>
2025-09-04 19:21:15 +0900

    feat : 리뷰 좋아요 추가 제거 api 구현

--- src/main/java/subscribenlike/mogupick/review/common/ReviewSuccessCode.java
+++ src/main/java/subscribenlike/mogupick/review/common/ReviewSuccessCode.java
@@ -10,7 +10,9 @@ import subscribenlike.mogupick.common.success.SuccessCode;
 public enum ReviewSuccessCode implements SuccessCode {
 
     REVIEW_CREATED(HttpStatus.CREATED, "리뷰가 성공적으로 작성되었습니다."),
-    PRODUCT_REVIEWS_FETCHED(HttpStatus.OK, "상품의 리뷰 목록을 조회하였습니다.");
+    PRODUCT_REVIEWS_FETCHED(HttpStatus.OK, "상품의 리뷰 목록을 조회하였습니다."),
+    REVIEW_LIKE_ADDED(HttpStatus.CREATED, "리뷰에 좋아요를 추가하였습니다."),
+    REVIEW_LIKE_REMOVED(HttpStatus.OK, "리뷰 좋아요를 제거하였습니다.");
 
     private final HttpStatus status;
     private final String message;
