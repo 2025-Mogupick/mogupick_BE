118179f7222e5e2185c81e8d07937df83dfdcb9a
0tae <choiyt3465@naver.com>
2025-09-04 11:15:46 +0900

    fix : 이미지를 리스트로 입력받을 수 있도록 수정

--- src/main/java/subscribenlike/mogupick/review/model/CreateReviewRequest.java
+++ src/main/java/subscribenlike/mogupick/review/model/CreateReviewRequest.java
@@ -1,23 +1,17 @@
 package subscribenlike.mogupick.review.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class CreateReviewRequest {
     private Long memberId;
     private Long productId;
     private String content;
     private double score;
-    private MultipartFile reviewImage;
-
-    public CreateReviewRequest(Long memberId, Long productId, String content, double score, MultipartFile reviewImage) {
-        this.memberId = memberId;
-        this.productId = productId;
-        this.content = content;
-        this.score = score;
-        this.reviewImage = reviewImage;
-    }
+    private List<MultipartFile> images;
 }
