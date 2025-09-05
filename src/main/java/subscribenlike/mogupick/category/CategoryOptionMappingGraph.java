a5e60927ad1b68e23ca3aaa3af14eadd3c9a2c2b
0tae <choiyt3465@naver.com>
2025-09-06 00:13:43 +0900

    refactor : 카테고리 구조 리팩토링 및 옵션 명 추가, 서브 카테고리명 수정

--- src/main/java/subscribenlike/mogupick/category/CategoryOptionMappingGraph.java
+++ src/main/java/subscribenlike/mogupick/category/CategoryOptionMappingGraph.java
@@ -1,161 +1,93 @@
 package subscribenlike.mogupick.category;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.stereotype.Component;
+import subscribenlike.mogupick.category.config.*;
+import subscribenlike.mogupick.category.config.convenience_food.ConvenienceFoodCategoryOptionConfig;
+import subscribenlike.mogupick.category.config.daily_goods.DailyGoodsCategoryOptionConfig;
+import subscribenlike.mogupick.category.config.dairy_beverage.DairyBeverageCategoryOptionConfig;
+import subscribenlike.mogupick.category.config.health_supplements.HealthSupplementsCategoryOptionConfig;
+import subscribenlike.mogupick.category.config.hygiene.HygieneCategoryOptionConfig;
+import subscribenlike.mogupick.category.config.meat_seafood.MeatSeafoodCategoryOptionConfig;
+import subscribenlike.mogupick.category.config.PetSuppliesCategoryOptionConfig;
+import subscribenlike.mogupick.category.config.snack.SnackCategoryOptionConfig;
+import subscribenlike.mogupick.category.config.fresh_food.FreshFoodCategoryOptionConfig;
 import subscribenlike.mogupick.category.domain.CategoryOption;
-import subscribenlike.mogupick.category.domain.CategoryOptionFilter;
 import subscribenlike.mogupick.category.domain.RootCategory;
+import subscribenlike.mogupick.category.domain.SubCategory;
+import subscribenlike.mogupick.category.config.baby_supplies.BabySuppliesCategoryOptionConfig;
 import subscribenlike.mogupick.category.model.CategoryOptionAndFilterResponse;
 import subscribenlike.mogupick.category.model.CategoryOptionResponse;
+import subscribenlike.mogupick.category.model.CategoryOptionsResponse;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Component
 public class CategoryOptionMappingGraph {
 
-    private final Map<RootCategory, ArrayList<CategoryOptionMappingNode>> graph;
+    private Map<RootCategory, CategoryOptionConfig> configs;
 
     private CategoryOptionMappingGraph() {
-        this.graph = new ConcurrentHashMap<>();
         init();
     }
 
     public void init() {
-        graphInit();
-        optionsInit();
-        optionFiltersInit();
-    }
-
-    private void graphInit() {
-        // 루트 카테고리 별 옵션 리스트 생성
-        Arrays.stream(RootCategory.values())
-                .forEach(rootCategory -> graph.put(rootCategory, new ArrayList<>()));
-    }
-
-    private void optionFiltersInit() {
-        // 가격 범위 필터 (범위 + 선택)
-        addFilters(RootCategory.CONVENIENCE_FOOD, CategoryOption.PRICE,
-                CategoryOptionFilter.of("10,000원 미만", "(0,10000)"),
-                CategoryOptionFilter.of("10,000원 이상-20,000원 미만", "[10000,20000)"),
-                CategoryOptionFilter.of("20,000원 이상-30,000원 미만", "[20000,30000)"),
-                CategoryOptionFilter.of("30,000원 이상-40,000원 미만", "[30000,40000)"),
-                CategoryOptionFilter.of("40,000원 이상", "[40000,)")
+        this.configs = Map.of(
+                RootCategory.FRESH_FOOD, new FreshFoodCategoryOptionConfig(),
+                RootCategory.CONVENIENCE_FOOD, new ConvenienceFoodCategoryOptionConfig(),
+                RootCategory.MEAT_SEAFOOD, new MeatSeafoodCategoryOptionConfig(),
+                RootCategory.DAIRY_BEVERAGE, new DairyBeverageCategoryOptionConfig(),
+                RootCategory.SNACK, new SnackCategoryOptionConfig(),
+                RootCategory.HEALTH_SUPPLEMENTS, new HealthSupplementsCategoryOptionConfig(),
+                RootCategory.DAILY_GOODS, new DailyGoodsCategoryOptionConfig(),
+                RootCategory.HYGIENE, new HygieneCategoryOptionConfig(),
+                RootCategory.PET_SUPPLIES, new PetSuppliesCategoryOptionConfig(),
+                RootCategory.BABY_SUPPLIES, new BabySuppliesCategoryOptionConfig()
         );
-
-        // 별점 갯수 필터 (단일 선택)
-        addFilters(RootCategory.CONVENIENCE_FOOD, CategoryOption.RATING,
-                CategoryOptionFilter.of("1개", "1"),
-                CategoryOptionFilter.of("2개", "2"),
-                CategoryOptionFilter.of("3개", "3"),
-                CategoryOptionFilter.of("4개", "4"),
-                CategoryOptionFilter.of("5개", "5")
-        );
-
-        // 중량 필터 (복수 선택)
-        addFilters(RootCategory.CONVENIENCE_FOOD, CategoryOption.WEIGHT,
-                CategoryOptionFilter.of("200g 이하", "(0,200]"),
-                CategoryOptionFilter.of("200g-500g", "(200,500]"),
-                CategoryOptionFilter.of("500g-1kg", "(500,1000]"),
-                CategoryOptionFilter.of("1kg-2kg", "(1000,2000]"),
-                CategoryOptionFilter.of("2kg-3kg", "(2000,3000]"),
-                CategoryOptionFilter.of("3kg 이상", "(3000,)")
-        );
-
-        // 간편식 섭취 방법 필터 (복수 선택)
-        addFilters(RootCategory.CONVENIENCE_FOOD, CategoryOption.EAT_METHOD,
-                CategoryOptionFilter.of("즉석완조리식품", "즉석완조리식품"),
-                CategoryOptionFilter.of("즉석섭취식품", "즉석섭취식품"),
-                CategoryOptionFilter.of("즉석반조리식품", "즉석반조리식품")
-        );
-
-        // 칼로리 필터 (복수 선택)
-        addFilters(RootCategory.CONVENIENCE_FOOD, CategoryOption.CALORIE,
-                CategoryOptionFilter.of("100kcal 이하", "(0,100]"),
-                CategoryOptionFilter.of("100kcal-200kcal", "(100,200]"),
-                CategoryOptionFilter.of("200kcal-300kcal", "(200,300]"),
-                CategoryOptionFilter.of("300kcal-500kcal", "(300,500]"),
-                CategoryOptionFilter.of("500kcal 이상", "(500,)")
-        );
-
-        // 에어프라이어 전자렌지 조리 가능 필터 (복수 선택)
-        addFilters(RootCategory.CONVENIENCE_FOOD, CategoryOption.COOK_METHOD,
-                CategoryOptionFilter.of("에어프라이어", "에어프라이어"),
-                CategoryOptionFilter.of("에어프라이어,전자렌지", "에어프라이어,전자렌지"),
-                CategoryOptionFilter.of("전자렌지", "전자렌지")
-        );
-
-        // 개당 수량 필터 (복수 선택)
-        addFilters(RootCategory.CONVENIENCE_FOOD, CategoryOption.QUANTITY,
-                CategoryOptionFilter.of("6개 이하", "(0,6]"),
-                CategoryOptionFilter.of("6개-10개", "(6,10]"),
-                CategoryOptionFilter.of("10개-20개", "(10,20]"),
-                CategoryOptionFilter.of("20개-30개", "(20,30]"),
-                CategoryOptionFilter.of("50개 이상", "[50,)")
-        );
-    }
-
-    private void optionsInit() {
-        addAllOptions(
-                RootCategory.CONVENIENCE_FOOD,
-                CategoryOption.PRICE,
-                CategoryOption.RATING,
-                CategoryOption.WEIGHT,
-                CategoryOption.CALORIE,
-                CategoryOption.QUANTITY,
-                CategoryOption.EAT_METHOD,
-                CategoryOption.COOK_METHOD);
     }
 
     public List<CategoryOption> getOptionsByRootCategory(RootCategory rootCategory) {
-        return graph.get(rootCategory).stream()
-                .map(CategoryOptionMappingNode::getCategoryOption)
-                .toList();
+        return configs.get(rootCategory).getCategoryOptions();
+    }
+
+    public List<CategoryOption> getOptionsBySubCategory(SubCategory subCategory) {
+        return configs.get(subCategory.getRoot()).getCategoryOptions(subCategory);
+    }
+
+    public List<CategoryOptionAndFilterResponse> getOptionAndFiltersBySubCategory(SubCategory subCategory) {
+        return configs.get(subCategory.getRoot()).getCategoryFilters(subCategory)
+                .entrySet().stream()
+                .map(optionFilters ->
+                        CategoryOptionAndFilterResponse.of(
+                                CategoryOptionResponse.from(optionFilters.getKey()),
+                                optionFilters.getValue())
+                ).toList();
     }
 
     public List<CategoryOptionAndFilterResponse> getOptionAndFiltersByRootCategory(RootCategory rootCategory) {
-        return graph.get(rootCategory).stream()
-                .map(node ->
+        return configs.get(rootCategory).getCategoryFilters()
+                .entrySet().stream()
+                .map((optionFilters) ->
                         CategoryOptionAndFilterResponse.of(
-                                CategoryOptionResponse.from(node.getCategoryOption()),
-                                node.getFilters()
-                        ))
-                .toList();
+                                CategoryOptionResponse.from(optionFilters.getKey()),
+                                optionFilters.getValue())
+                ).toList();
     }
 
-    private void addAllOptions(RootCategory rootCategory, CategoryOption... options) {
-        Arrays.stream(options).forEach(option ->
-                graph.get(rootCategory).add(CategoryOptionMappingNode.of(rootCategory, option))
-        );
-    }
+    public CategoryOptionsResponse getAllOptionsAndFiltersByRootCategory(RootCategory rootCategory) {
+        // RootCategory의 옵션과 필터 (common)
+        List<CategoryOptionAndFilterResponse> common = getOptionAndFiltersByRootCategory(rootCategory);
 
-    private void addFilters(RootCategory rootCategory,
-                            CategoryOption categoryOption,
-                            CategoryOptionFilter... filters) {
+        // 각 SubCategory의 옵션과 필터
+        Map<SubCategory, List<CategoryOptionAndFilterResponse>> subCategories =
+                SubCategory.findByRootCategory(rootCategory).stream()
+                        .collect(Collectors.toMap(
+                                subCategory -> subCategory,
+                                this::getOptionAndFiltersBySubCategory
+                        ));
 
-        CategoryOptionMappingNode node = graph.get(rootCategory).stream()
-                .filter(n -> n.getCategoryOption() == categoryOption)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(
-                        "%s 에 %s 옵션이 존재하지 않습니다.".formatted(rootCategory.name(), categoryOption.name())));
-
-        node.addFilter(filters);
-    }
-}
-
-@Getter
-@AllArgsConstructor(staticName = "of")
-class CategoryOptionMappingNode {
-    private RootCategory rootCategory;
-    private CategoryOption categoryOption;
-    private final ArrayList<CategoryOptionFilter> filters = new ArrayList<>();
-
-    public void addFilter(CategoryOptionFilter... optionFilters) {
-        filters.addAll(Arrays.asList(optionFilters));
+        return CategoryOptionsResponse.of(common, subCategories);
     }
 }
 
