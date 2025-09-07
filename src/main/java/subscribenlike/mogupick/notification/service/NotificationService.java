954cb6434121ba2353c332652d603693790ff6e0
SeongJun98 <seongjunin98@gmail.com>
2025-09-08 03:22:40 +0900

    feat: 알림 API를 위한 서비스 구현

--- src/main/java/subscribenlike/mogupick/notification/service/NotificationService.java
+++ src/main/java/subscribenlike/mogupick/notification/service/NotificationService.java
@@ -4,9 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import subscribenlike.mogupick.member.domain.Member;
+import subscribenlike.mogupick.notification.common.exception.NotificationErrorCode;
+import subscribenlike.mogupick.notification.common.exception.NotificationException;
 import subscribenlike.mogupick.notification.domain.Notification;
 import subscribenlike.mogupick.notification.domain.NotificationType;
+import subscribenlike.mogupick.notification.dto.NotificationResponse;
 import subscribenlike.mogupick.notification.repository.NotificationRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -23,5 +29,24 @@ public class NotificationService {
                 .url(url)
                 .build();
         notificationRepository.save(notification);
+    }
+
+    @Transactional(readOnly = true)
+    public List<NotificationResponse> getNotifications(Long memberId) {
+        List<Notification> notifications = notificationRepository.findByMemberIdOrderByCreatedAtDesc(memberId);
+        return notifications.stream()
+                .map(NotificationResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public void readNotification(Long notificationId, Long memberId) {
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(() -> new NotificationException(NotificationErrorCode.NOTIFICATION_NOT_FOUND));
+
+        if (!notification.getMember().getId().equals(memberId)) {
+            throw new NotificationException(NotificationErrorCode.FORBIDDEN_READ_NOTIFICATION);
+        }
+
+        notification.read();
     }
 }
