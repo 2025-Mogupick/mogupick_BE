5e59de39b2a2344c98226b974bcf4682a59fc5c3
SeongJun98 <seongjunin98@gmail.com>
2025-09-08 01:33:00 +0900

    feat: 알림 도메인 엔티티 및 리포지토리 추가

--- src/main/java/subscribenlike/mogupick/notification/domain/NotificationType.java
+++ src/main/java/subscribenlike/mogupick/notification/domain/NotificationType.java
@@ -1,6 +1,16 @@
 package subscribenlike.mogupick.notification.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum NotificationType {
-    WARNING;
-    // todo 내용 추가논의 필요
+
+    DELIVERY_STARTED("배송 시작 알림"),
+    PAYMENT_REMINDER("결제 예정 알림"),
+    PAYMENT_COMPLETED("결제 완료 알림"),
+    NEW_REVIEW("새로운 리뷰 알림");
+
+    private final String description;
 }
