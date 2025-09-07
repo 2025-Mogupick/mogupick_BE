9190f99a7caccd510c87900518f2f854818a6c59
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-09-08 03:01:02 +0900

    feat: 구독 관리 관련 repository 추가

---
+++ src/main/java/subscribenlike/mogupick/subscription/repository/SubscriptionRepository.java
@@ -0,0 +1,15 @@
+package subscribenlike.mogupick.subscription.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import subscribenlike.mogupick.subscription.domain.Subscription;
+import subscribenlike.mogupick.subscription.domain.SubscriptionStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    List<Subscription> findByMemberId(Long memberId);
+    List<Subscription> findByMemberIdAndStatus(Long memberId, SubscriptionStatus status);
+    Optional<Subscription> findByPaymentKey(String paymentKey);
+    boolean existsByPaymentKey(String paymentKey);
+}
