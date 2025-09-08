7fea3ee401c65cb15eeb1b5764c0f684550b91e6
SeongJun98 <seongjunin98@gmail.com>
2025-09-08 17:39:41 +0900

    refactor: 권한 기반 API 접근 제어 수정

--- src/main/java/subscribenlike/mogupick/global/config/SecurityConfig.java
+++ src/main/java/subscribenlike/mogupick/global/config/SecurityConfig.java
@@ -36,8 +36,25 @@ public class SecurityConfig {
                 .formLogin(form -> form.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        // TODO: 개발 완료 후, 기존 인증 설정으로 변경.
-                        .requestMatchers("/**").permitAll()
+                                // swagger, 소셜 로그인/재발급 API 등은 인증 없이 누구나 접근 가능
+                                .requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/api/v1/auth/social-login",
+                                        "/api/v1/auth/reissue"
+                                ).permitAll()
+
+                                // '/members/me', '/notifications/**' 등은 인증된 사용자만 접근 가능
+                                .requestMatchers(
+                                        "/api/v1/members/me",
+                                        "/api/v1/notifications/**"
+                                ).authenticated()
+
+                                // 로그아웃도 인증된 사용자만 가능
+                                .requestMatchers("/api/v1/auth/logout").authenticated()
+
+                                // 위 경로 외 나머지 모든 요청은 허용
+                                .anyRequest().permitAll()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
