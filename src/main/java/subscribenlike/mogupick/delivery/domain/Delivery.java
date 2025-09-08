04a21cf48db0e3ff2758b406135b3b17ab552b77
SeongJun98 <seongjunin98@gmail.com>
2025-09-08 17:37:37 +0900

    feat: 배송 도메인 구현

---
+++ src/main/java/subscribenlike/mogupick/delivery/domain/Delivery.java
@@ -0,0 +1,42 @@
+package subscribenlike.mogupick.delivery.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import subscribenlike.mogupick.common.domain.BaseEntity;
+import subscribenlike.mogupick.member.domain.Member;
+import subscribenlike.mogupick.product.domain.Product;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Delivery extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @Builder
+    public Delivery(Member member, Product product) {
+        this.member = member;
+        this.product = product;
+        this.status = DeliveryStatus.PENDING;
+    }
+
+    public void start() {
+        this.status = DeliveryStatus.SHIPPED;
+    }
+}
