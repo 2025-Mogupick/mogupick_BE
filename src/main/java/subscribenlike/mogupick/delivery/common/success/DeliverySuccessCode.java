9c24ec987e0f524b6066fcb72651fb4bf65e0c94
SeongJun98 <seongjunin98@gmail.com>
2025-09-08 22:03:48 +0900

    feat: 배송완료 이넘 추가

--- src/main/java/subscribenlike/mogupick/delivery/common/success/DeliverySuccessCode.java
+++ src/main/java/subscribenlike/mogupick/delivery/common/success/DeliverySuccessCode.java
@@ -10,7 +10,8 @@ import subscribenlike.mogupick.common.success.SuccessCode;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum DeliverySuccessCode implements SuccessCode {
 
-    DELIVERY_STARTED_SUCCESS(HttpStatus.OK, "배송 시작 처리에 성공했습니다.");
+    DELIVERY_STARTED_SUCCESS(HttpStatus.OK, "배송 시작 처리에 성공했습니다."),
+    DELIVERY_COMPLETED_SUCCESS(HttpStatus.OK, "배송 완료 처리에 성공했습니다.");
 
     private final HttpStatus status;
     private final String message;
