fcd75739eac0c095b729dc853918c96b18a7f034
0tae <choiyt3465@naver.com>
2025-09-10 12:15:25 +0900

    refactor : 상품에 맞는 옵션이 없다면 null을 반환하도록 수정

--- src/main/java/subscribenlike/mogupick/product/repository/ProductOptionRepository.java
+++ src/main/java/subscribenlike/mogupick/product/repository/ProductOptionRepository.java
@@ -3,6 +3,7 @@ package subscribenlike.mogupick.product.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import subscribenlike.mogupick.category.domain.RootCategory;
 import subscribenlike.mogupick.category.domain.SubCategory;
+import subscribenlike.mogupick.common.utils.GlobalLogger;
 import subscribenlike.mogupick.product.common.ProductErrorCode;
 import subscribenlike.mogupick.product.common.ProductException;
 import subscribenlike.mogupick.product.domain.Product;
@@ -14,12 +15,15 @@ import java.util.Optional;
 public interface ProductOptionRepository extends MongoRepository<ProductOption, String> {
 
     List<ProductOption> findAllByRootCategory(RootCategory rootCategory);
+
     Optional<ProductOption> findByProductId(Long productId);
 
     List<ProductOption> findAllBySubCategory(SubCategory subCategory);
 
     default ProductOption getByProductId(Long productId) {
-        return findByProductId(productId).
-                orElseThrow(() -> new ProductException(ProductErrorCode.PRODUCT_NOT_FOUND));
+        return findByProductId(productId).orElseGet(()->{
+            GlobalLogger.warn(ProductErrorCode.PRODUCT_NOT_FOUND.getMessage());
+            return null;
+        });
     }
 }
