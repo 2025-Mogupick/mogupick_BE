a0a1feed06243943bcef93dcad4b14d8a074e645
0tae <choiyt3465@naver.com>
2025-09-11 17:46:12 +0900

    refactor : 토큰 로그 정보 추가

--- src/main/java/subscribenlike/mogupick/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/subscribenlike/mogupick/global/jwt/JwtAuthenticationFilter.java
@@ -61,6 +61,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
+        GlobalLogger.info("Token :", bearerToken);
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
