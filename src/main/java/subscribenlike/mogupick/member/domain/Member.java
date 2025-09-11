b9cf2de8b06d04a11911b00aa841ec83fc23b765
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-09-11 09:38:20 +0900

    fix: 멤버 엔티티에 customKey 컬럼 추가

--- src/main/java/subscribenlike/mogupick/member/domain/Member.java
+++ src/main/java/subscribenlike/mogupick/member/domain/Member.java
@@ -3,6 +3,8 @@ package subscribenlike.mogupick.member.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.UUID;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -42,9 +44,13 @@ public class Member extends BaseEntity {
 
     private String profileImage;
 
+    @Column(unique = true)
+    private String customerKey;
+
     @Builder
     public Member(Long id, String name, String email, String password, String phoneNumber, LocalDate birthDate,
                   boolean isAccepted, MemberRole role, String provider, String nickname, String profileImage) {
+
         this.id = id;
         this.name = name;
         this.email = email;
@@ -76,6 +82,12 @@ public class Member extends BaseEntity {
     public void updateNickname(String nickname) {
         if (nickname != null && !nickname.isBlank()) {
             this.nickname = nickname;
+        }
+    }
+
+    public void ensureCustomerKey() {
+        if (this.customerKey == null) {
+            this.customerKey = UUID.randomUUID().toString();
         }
     }
   
